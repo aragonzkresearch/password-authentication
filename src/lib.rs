72d57c5a30ec3116cd56ea0e5ab41e910b0b679d
dmvict <dm.vict.kr@gmail.com>
2024-02-12 12:34:06 +0200

    Update `GroupDescription` constructor, copy tests, remove duplication of module in subdirectory

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,8 @@ use sha3::{Digest, Sha3_256 as HashFunc};
 
 #[derive(Default, Debug, CanonicalDeserialize, CanonicalSerialize)]
 pub struct Proof<C: Group> {
-    pub g: C, // maybe it's extra field. I think we can use enum and construct it on the fly.
+    /// Generic generator that can be reused in destination handler.
+    pub g: C,
     /// Random value from which calculates commit and z.
     pub rand: String,
     pub commit: C,
@@ -23,10 +24,8 @@ pub struct GroupDescription<C: CurveGroup> {
 
 impl<C: CurveGroup> GroupDescription<C> {
     /// Constructor of the instance.
-    pub fn new() -> GroupDescription<C> {
-        GroupDescription {
-            generator: C::generator(),
-        }
+    pub fn new(generator: C) -> GroupDescription<C> {
+        GroupDescription { generator }
     }
 
     /// A helper routine that generate keypair at once.
@@ -127,4 +126,89 @@ fn hash<C: CurveGroup>(x: &C, y: &C, r: &str) -> Result<<C as Group>::ScalarFiel
 
 fn hex_to_bignum(hex_str: &str) -> Result<BigUint, ParseBigIntError> {
     BigUint::from_str_radix(hex_str, 16)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ark_bls12_381::G1Projective as tlcs_curve_bls;
+    use ark_ed_on_bn254::EdwardsProjective as tlcs_curve_bjj;
+    use ark_secp256k1::Projective as tlcs_curve_secp;
+    use ark_std::rand::Rng;
+
+    const PW_SIZE: usize = 16;
+    const SALT_SIZE: usize = 16;
+
+    fn generate_password() -> [u8; PW_SIZE] {
+        let mut rng = rand::thread_rng();
+        let mut array = [0u8; PW_SIZE];
+        rng.fill(&mut array);
+        array
+    }
+
+    fn generate_random_salt() -> [u8; SALT_SIZE] {
+        // Generate SALT
+        let mut rng = rand::thread_rng();
+        let mut salt = [0u8; SALT_SIZE];
+        rng.fill(&mut salt);
+        salt
+    }
+
+    pub fn get_randomness() -> &'static str {
+        "17281ff777148ec841fa6dd86c42ad049c04612f3948187a47fb0fe280f389fd"
+    }
+
+    #[test]
+    fn test_bjj_works() {
+        for _i in 0..10 {
+            let g = tlcs_curve_bjj::generator();
+            let group_description = GroupDescription::new(g); //Question: is there any other way to define this?
+            let pw = generate_password();
+            let salt = generate_random_salt();
+            let (sk, pk) = group_description
+                .clone()
+                .generate_keypair(&pw, &salt)
+                .unwrap();
+            let proof: Proof<tlcs_curve_bjj> =
+                group_description.proof(pk, sk, get_randomness()).unwrap();
+            let vrf = GroupDescription::<tlcs_curve_bjj>::verify_proof(&proof, &pk);
+            assert!(vrf);
+        }
+    }
+
+    #[test]
+    fn test_secp_works() {
+        for _i in 0..10 {
+            let g = tlcs_curve_secp::generator();
+            let group_description = GroupDescription::new(g); //Question: is there any other way to define this?
+            let pw = generate_password();
+            let salt = generate_random_salt();
+            let (sk, pk) = group_description
+                .clone()
+                .generate_keypair(&pw, &salt)
+                .unwrap();
+            let proof: Proof<tlcs_curve_secp> =
+                group_description.proof(pk, sk, get_randomness()).unwrap();
+            let vrf = GroupDescription::<tlcs_curve_secp>::verify_proof(&proof, &pk);
+            assert!(vrf);
+        }
+    }
+
+    #[test]
+    fn test_bls_works() {
+        for _i in 0..10 {
+            let g = tlcs_curve_bls::generator();
+            let group_description = GroupDescription::new(g); //Question: is there any other way to define this?
+            let pw = generate_password();
+            let salt = generate_random_salt();
+            let (sk, pk) = group_description
+                .clone()
+                .generate_keypair(&pw, &salt)
+                .unwrap();
+            let proof: Proof<tlcs_curve_bls> =
+                group_description.proof(pk, sk, get_randomness()).unwrap();
+            let vrf = GroupDescription::<tlcs_curve_bls>::verify_proof(&proof, &pk);
+            assert!(vrf);
+        }
+    }
 }
